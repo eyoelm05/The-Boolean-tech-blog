ecf986d0097d28b46bddb650edb4fdc7b1cffb05
Eyoel Mekonnen <l39668304@student.ua92.ac.uk>
2025-10-23 09:03:38 +0100

    feat: Modify latest section to use dynamic content
    Add pictures for latest section. Modify index.html and HomePage.css to apply the change.
    Dynamically render using javascript.

--- js/HomePage.js
+++ js/HomePage.js
@@ -2,6 +2,7 @@
 /* Fetch Element from DOM */
 const carouselContainer = document.getElementById('carousel-inner');
 const trendingContainer = document.getElementById('trending-cards');
+const latestContainer = document.getElementById('latest-cards');
 
 /* Loop over items in main and add them to the carousel Container */
 data.forEach((element, index) => {
@@ -36,12 +37,34 @@ data.forEach((element) => {
                             <p class="date"><time datetime="${new Date(element.date)}">${element.date}</time></p>
                         </section>
                         <figure class="image-container">
-                            <img src="images/${element.image_name}" alt="${element.image_url}" class="card-image">
+                            <img src="images/${element.image_name}" alt="${element.image_alt}" class="card-image">
                         </figure>
                     </section>
-                <a>
+                </a>
             `
         trendingContainer.innerHTML += cardElement
+    }
+})
+
+data.forEach((element) => {
+    if(element.type === "latest"){
+        /* Prepare card element */
+        const cardElement = `
+                <a href="html/SingleBlog.html?id=${element.id}">
+                    <section class="card-local">
+                        <section class="card-content">
+                            <p class="card-badge">${element.category}</p>
+                            <h1 class="title card-title">${element.title}</h1>
+                            <p class="description card-description">${element.shortDescription}</p>
+                            <p class="date"><time datetime="${new Date(element.date)}">${element.date}</time></p>
+                        </section>
+                        <figure class="image-container">
+                            <img src="images/${element.image_name}" alt="${element.image_alt}" class="card-image">
+                        </figure>
+                    </section>
+                </a>
+            `
+        latestContainer.innerHTML += cardElement
     }
 })
 
