bda5aa24814281700a93956f4708b6b94a0a96b7
Eyoel Mekonnen <l39668304@student.ua92.ac.uk>
2025-10-27 12:25:42 +0000

    fix: Change auto-scroll carousel to button control

--- js/Carousel.js
+++ js/Carousel.js
@@ -3,6 +3,3 @@ const CarouselElement = document.getElementById('main-carousel');
 const carousel = new bootstrap.Carousel(CarouselElement, {
     interval: 7000,
 });
-
-/* Start the auto playing carousel */
-carousel.cycle();
