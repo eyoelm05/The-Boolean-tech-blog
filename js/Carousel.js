3fd2c967ad7a9d817c6896287ac663feb55431e0
Eyoel Mekonnen <l39668304@student.ua92.ac.uk>
2025-10-31 13:18:18 +0000

    fix: Delete Carousel.js
    Carousel.js is deleted as auto-loading carousel is no longer needed.

--- js/Carousel.js
+++
@@ -1,5 +0,0 @@
-/* Manually initializing the carousel */
-const CarouselElement = document.getElementById('main-carousel');
-const carousel = new bootstrap.Carousel(CarouselElement, {
-    interval: 7000,
-});
